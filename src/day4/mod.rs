6f93c7b5bcd2db42e85d109e9baea219c00905da
Galen Elias <gelias@gmail.com>
2021-12-04 21:44:53 -0800

    Day 4 Rust Cleanup (more functional)

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -1,82 +1,55 @@
 use itertools::Itertools;
 
-fn check_solved(marks: &Vec<Vec<bool>>) -> bool {
-	for i in 0..5 {
-		let mut count_row = 0;
-		let mut count_col = 0;
-
-		for j in 0..5 {
-			if marks[i][j] {
-				count_row += 1;
-			}
-			if marks[j][i] {
-				count_col += 1;
-			}
-		}
-		if count_row == 5 || count_col == 5 {
-			return true;
-		}
-	}
-
-	return false;
+struct BingoSquare {
+	value: u32,
+	marked: bool,
 }
 
-fn sum_unmarked(board: &Vec<Vec<u32>>, marked: &Vec<Vec<bool>>) -> u32 {
-	let mut sum = 0;
-	for r in 0..5 {
-		for c in 0..5 {
-			if !marked[r][c] {
-				sum += board[r][c];
-			}
-		}
-	}
-	return sum;
+type Board = Vec<Vec<BingoSquare>>;
+
+fn check_solved(board: &Board) -> bool {
+	board.iter().any(|row| row.iter().all(|square| square.marked))
+	|| (0..5).any(|col| board.iter().all(|row| row[col].marked))
+}
+
+fn sum_unmarked(board: &Board) -> u32 {
+	board.iter().flatten().filter(|square| !square.marked).map(|square| square.value).sum()
 }
 
 pub fn solve(inputs: Vec<String>) {
 	let draws = inputs[0].split(",").map(|s| s.parse::<u32>().unwrap()).collect_vec();
 
-	let mut boards: Vec<Vec<Vec<u32>>> = Vec::new();
-	let mut marks: Vec<Vec<Vec<bool>>> = Vec::new();
+	let mut boards = inputs[2..].chunks(6).map(|chunk| {
+		chunk[0..5].iter().map(|line|
+			line.split_whitespace().map(|s| BingoSquare { value: s.parse::<u32>().unwrap(), marked: false }).collect_vec()
+		).collect_vec()
+	}).collect_vec();
 
-	for b in 0..(inputs.len()-2)/6 {
-		let mut board: Vec<Vec<u32>> = Vec::new();
-		for r in 0..5 {
-			board.push(inputs[2 + b * 6 + r].split_whitespace().map(|s| s.parse::<u32>().unwrap()).collect_vec());
+	let original_size = boards.len();
+
+	for draw in draws {
+		for board in &mut boards {
+			for square in board.iter_mut().flatten() {
+				if square.value == draw {
+					square.marked = true;
+				}
+			}
 		}
 
-		boards.push(board);
-		marks.push(vec![vec![false; 5]; 5]);
-	}
-
-	let mut solved_boards = vec![false; boards.len()];
-	let mut solved_boards_count = 0;
-
-	let mut part1_solved = false;
-	'outer: for draw in draws {
-		for b in 0..boards.len() {
-			for r in 0..5 {
-				for c in 0..5 {
-					if boards[b][r][c] == draw {
-						marks[b][r][c] = true;
-					}
-				}
+		if let Some(winner) = boards.iter().find(|board| check_solved(board)) {
+			if boards.len() == original_size {
+				println!("Part 1: {}", draw * sum_unmarked(winner));
 			}
 
-			if !solved_boards[b] && check_solved(&marks[b]) {
-				solved_boards[b] = true;
-				solved_boards_count += 1;
-
-				if !part1_solved {
-					println!("Part 1: {}, {}, {}", draw, b, draw * sum_unmarked(&boards[b], &marks[b]));
-					part1_solved = true;
-				}
-
-				if solved_boards_count == boards.len() {
-					println!("Part 2: {}, {}, {}", draw, b, draw * sum_unmarked(&boards[b], &marks[b]));
-					break 'outer;
-				}
+			if boards.len() == 1 {
+				println!("Part 2: {}", draw * sum_unmarked(&boards[0]));
 			}
+		}
+
+		boards.retain(|board| !check_solved(board));
+
+		if boards.len() == 0 {
+			break;
 		}
 	}
 }
